29a5138ee6e243d8e733d12edd4632193d50874d
opeyemi <oropo015@yahoo.com>
2024-06-29 09:35:24 +0100

    Edit button updatedv

--- compressor.py
+++ compressor.py
@@ -1,12 +1,23 @@
 import PySimpleGUI as sg
+from  function import make_achieve as mk
 
-label1 = sg.Text("Source Folder     :")
+label1 = sg.Text("Source Folder:    ")
 t_box1 = sg.InputText()
+choose1 = sg.FilesBrowse("Choose")
 
 label2 = sg.Text("Destination Folder:")
 t_box2 = sg.InputText()
+choose2 = sg.FolderBrowse("Choose")
 
 compress = sg.Button("Compress")
-window = sg.Window("Compressor", [[label1, t_box1], [label2, t_box2], [compress]])
-window.read()
+window = sg.Window("Compressor", [[label1, t_box1, choose1], [label2, t_box2, choose2], [compress]])
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    filepaths = value[0].split(";")
+    dest = value[1]
+    mk(filepaths, dest)
+
+
 window.close()
--- function.py
+++ function.py
@@ -1,3 +1,7 @@
+import zipfile
+import pathlib
+
+
 def get_todos():
     with open("todo.txt", "r") as file:
         todos_list = file.readlines()
@@ -7,3 +11,17 @@ def get_todos():
 def write_todos(todos_write):
     with open("todo.txt", "w") as file:
         todos_list = file.writelines(todos_write)
+
+
+def make_achieve(filepaths_, dest_):
+    dest_dir = pathlib.Path(dest_, "dest.zip")
+    with zipfile.ZipFile(dest_dir, 'w') as achieve:
+        for file in filepaths_:
+            file = pathlib.Path(file)
+            achieve.write(file, arcname=file.name)
+
+
+if __name__ == '__main__':
+    filepaths=["a.txt", "b.txt", "c.txt"]
+    dest = "dest"
+    make_achieve(filepaths, dest)
